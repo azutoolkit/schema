fc40b3af89da5773bfdee9b85fe5d3d9d49b4006
Elias Perez <eliasjpr@users.noreply.github.com>
2023-06-10 09:58:26 -0400

    Create local_converter.cr

---
+++ src/schema/ext/time/local_converter.cr
@@ -0,0 +1,25 @@
+require "../../annotations"
+
+@[Schema::Definition::Scalar]
+struct Time
+  module LocalConverter
+    FORMAT = "%Y-%m-%dT%H:%M"
+
+    # Put *value* as a time timestamp into the *builder* at *key*.
+    def self.to_http_param(value : Time, builder : HTTP::Params::Builder, key : String)
+      builder.add(key, to_http_param(value))
+    end
+
+    # Return *value* as a time timestamp string.
+    def self.to_http_param(value : Time)
+      value.to_s(format)
+    end
+
+    # Parse `Time` from an HTTP param as time timestamp.
+    def self.from_http_param(value : String) : Time
+      Time.parse(time, FORMAT)
+    rescue ArgumentError
+      raise TypeCastError.new
+    end
+  end
+end
